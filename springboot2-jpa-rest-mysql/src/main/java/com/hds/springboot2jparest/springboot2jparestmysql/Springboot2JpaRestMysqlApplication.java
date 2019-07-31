30e42200563790a1cd211b0071dc1a853c54fe43
sathiyanvenkatesh <sathiyan.venkatesh@1screen.com>
2019-07-31 18:23:56 +0400

    initail commit

---
+++ springboot2-jpa-rest-mysql/src/main/java/com/hds/springboot2jparest/springboot2jparestmysql/Springboot2JpaRestMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.hds.springboot2jparest.springboot2jparestmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2JpaRestMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot2JpaRestMysqlApplication.class, args);
+	}
+
+}
---
+++ springboot2-jpa-rest-mysql/src/test/java/com/hds/springboot2jparest/springboot2jparestmysql/Springboot2JpaRestMysqlApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hds.springboot2jparest.springboot2jparestmysql;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Springboot2JpaRestMysqlApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
